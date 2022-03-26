de60810aa8219e449325bc0cd823027b9ead65af
CTFang's Macbook <andy89923@gmail.com>
2022-03-26 19:34:49 +0800

    HW2 init

---
+++ HW2/mitm_attack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>    // inet_addr
+#include <netinet/ip.h>   // Provides declarations for ip header
+#include <netinet/udp.h>  // Provides declarations for udp header
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+
+    return 0;
+}
