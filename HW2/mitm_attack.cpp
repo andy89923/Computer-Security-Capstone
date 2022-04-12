29e6305a587350ceca77f42ca1e5494e4ef37f10
Ctfang CSC <ctfang.cs08@nycu.edu.tw>
2022-04-12 08:39:06 -0700

    HW2 Arp scan

--- HW2/mitm_attack.cpp
+++ HW2/mitm_attack.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <ctime>
 #include <iomanip>
+#include <thread>
 
 #include <netinet/if_ether.h>
 #include <netinet/in.h>
@@ -77,7 +78,7 @@ void getInfo(char* IP, unsigned char* MC, int& index) {
 	// cout << "Get Info Finished\n";
 }
 
-void print_result(char* dst_ip, char* rbuf) {
+void print_result(unsigned char* dst_ip, unsigned char* mac) {
 	string ss = "";
 	for (int j = 0; j < 4; j++) {
 		if (j != 0) ss = ss +  '.';
@@ -85,16 +86,17 @@ void print_result(char* dst_ip, char* rbuf) {
 	}
 	cout << setfill(' ') << setw(18) << left << ss;
 	
-	for (int j = 6; j < 6 + ETH_ALEN; j++) {
-		if (j != 6) cout << ":";
-		cout << setw(2) << setfill('0') << uppercase << hex << (unsigned int)((unsigned char) rbuf[j]);
+	for (int j = 0; j < ETH_ALEN; j++) {
+		if (j != 0) cout << ":";
+		cout << setw(2) << setfill('0') << uppercase << hex << (unsigned int)((unsigned char) mac[j]);
 	}
 	cout << '\n';
 }
 
 int finish_scan = 0;
+char src_ip[4];
 
-void arpscan_recv(int sock_r, char* src_ip) {
+void arpscan_recv(int sock_r) {
 	struct timeval tv = { 1, 0 };
 	if (setsockopt(sock_r, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
 		cout << "Error on recv_socket!\n";
@@ -103,7 +105,8 @@ void arpscan_recv(int sock_r, char* src_ip) {
 	unsigned char rbuf[80];
 	while (true) {
 		if (recvfrom(sock_r, rbuf, sizeof(rbuf), 0, NULL, NULL) < 0) {
-			if (finish_scan) break;
+			if (finish_scan == 3) break;
+			if (finish_scan) finish_scan += 1;
 			// cout << "Nothing got or error!\n";
 			continue;
 		}
@@ -111,13 +114,17 @@ void arpscan_recv(int sock_r, char* src_ip) {
 		// Check source ip
 		int ok = 1;
 		for (int i = 38, j = 0; i < 38 + 4; i++, j++) {
-			if (rbuf[i] != src_ip[j]) ok = false;
+			if ((unsigned int) rbuf[i] != (unsigned int) (unsigned char) src_ip[j]) {
+				ok = false;
+				// cout << j << ' ' << (unsigned int)rbuf[i] << ' ' << (unsigned int)(unsigned char) src_ip[j] << '\n';
+			}
 		}
 		// Check sender ip (target) != myself or Router
 		if (rbuf[31] == src_ip[3] || rbuf[31] == 1) ok = 0;
-		if (ok) continue;
+		if (!ok) continue;
 
-		print_result(rbuf + 38, rbuf + 32);
+		print_result(rbuf + 28, rbuf + 22);
+	
 	}
 }
 
@@ -131,7 +138,7 @@ int main(int argc, char const *argv[]) {
 	unsigned char dst_mac_addr[ETH_ALEN] = BROADCAST_ADDR;
 	unsigned char src_mac_addr[ETH_ALEN];
 	struct in_addr src_in_addr, dst_in_addr;
-	char src_ip[4], dst_ip[4];
+	char dst_ip[4];
 	int index;
 	
 	getInfo(src_ip, src_mac_addr, index);
@@ -169,9 +176,9 @@ int main(int argc, char const *argv[]) {
 
 
 	int sock_r;
-	socket_init(sock_r, src_ip);
+	socket_init(sock_r);
 
-    thread recevier(arpscan_recv, packet_socket);
+    thread recevier(arpscan_recv, sock_r);
     for (int i = 2; i < 254; i++) {
 		dst_ip[3] = i;
 		memcpy(arp_packet -> arp_tpa, dst_ip, IP_ADDR_LEN);
@@ -179,11 +186,12 @@ int main(int argc, char const *argv[]) {
 
 		if (sendto(sock_r, buf, ETHER_ARP_PACKET_LEN, 0, (struct sockaddr*) &send_addr, sizeof(send_addr)) < 0) {
 			cout << "Sendto Error!\n";	
-		}		
+		}
+		sleep(0.5);
 	}
+	sleep(3);
 	finish_scan = 1;
 	recevier.join();
-
 
 
 	close(sock_r);
