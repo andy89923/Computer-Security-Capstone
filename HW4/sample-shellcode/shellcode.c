4de6f1062cc90dc2e27a6f9e93debe3fe3858e83
CSC HW <ctfang.cs08@nycu.edu.tw>
2022-06-10 18:11:04 +0800

    HW4 Secret

--- HW4/sample-shellcode/shellcode.c
+++ HW4/sample-shellcode/shellcode.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 int main() {
-    char buff[0x100];
+    char buff[0x500];
     write(1, "input shellcode:", 16);
-    read(0, buff, 0x100);
+    read(0, buff, 0x500);
     void(* func)() = buff;
     func();
     return 0;
